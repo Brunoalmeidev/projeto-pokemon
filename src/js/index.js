a2423542ef942544cf4f6bb68a353d9c552bd8fb
Bruno Almeida <106546587+Brunoalmeidev@users.noreply.github.com>
2022-07-31 11:31:20 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,70 @@
+/* OBJETIVO 1 - quando clicarmos na seta de avancar temos que mostrar o proximo cartao da lista
+    - passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
+    - passo 2 - dar um jeito de identificar o clique do usuario na seta avancar
+    - passo 3 - fazer aparecer o proximo cartão da lista
+    - passo 4 - buscar o cartão que está selecionado e esconder
+
+
+    OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartao anterior da lista
+    - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+    - passo 2 - dar um jeito de identificar o clique do usuario na seta voltar
+    - passo 3 - fazer aparecer o cartão anterior da lista
+    - passo 4 - buscar o cartão que está selecionado e esconder
+
+*/
+
+/* OBJETIVO 1 - quando clicarmos na seta de avancar temos que mostrar o proximo cartao da lista
+    - passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
+    - passo 2 - dar um jeito de identificar o clique do usuario na seta avancar
+    - passo 3 - fazer aparecer o proximo cartão da lista
+    - passo 4 - buscar o cartão que está selecionado e esconder
+*/
+
+// passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado(){
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+
+}
+
+function mostrarCartao(indiceCartao){
+    cartoes[indiceCartao].classList.add("selecionado");
+
+}
+
+// passo 2 - dar um jeito de identificar o clique do usuario na seta avancar
+btnAvancar.addEventListener("click", function () {
+    if(cartaoAtual === cartoes.length - 1) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);
+
+});
+
+/* OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartao anterior da lista
+- passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+- passo 2 - dar um jeito de identificar o clique do usuario na seta voltar
+- passo 3 - fazer aparecer o cartão anterior da lista
+- passo 4 - buscar o cartão que está selecionado e esconder
+
+*/
+
+btnVoltar.addEventListener("click", function () {
+
+    if(cartaoAtual === 0) return;
+
+    esconderCartaoSelecionado();
+
+
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
+
+
+});
