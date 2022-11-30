5ba52ccca40cb41a2943a4051bed8d505c20782c
Bruno Almeida <106546587+Brunoalmeidev@users.noreply.github.com>
2022-11-30 19:15:09 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,23 +4,9 @@
     - passo 3 - fazer aparecer o proximo cartão da lista
     - passo 4 - buscar o cartão que está selecionado e esconder
 
-
-    OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartao anterior da lista
-    - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
-    - passo 2 - dar um jeito de identificar o clique do usuario na seta voltar
-    - passo 3 - fazer aparecer o cartão anterior da lista
-    - passo 4 - buscar o cartão que está selecionado e esconder
-
 */
 
-/* OBJETIVO 1 - quando clicarmos na seta de avancar temos que mostrar o proximo cartao da lista
-    - passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
-    - passo 2 - dar um jeito de identificar o clique do usuario na seta avancar
-    - passo 3 - fazer aparecer o proximo cartão da lista
-    - passo 4 - buscar o cartão que está selecionado e esconder
-*/
-
-// passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
+// passo 1 - pegar o elemento HTML da seta e avancar
 const btnAvancar = document.getElementById("btn-avancar");
 const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
@@ -37,7 +23,7 @@ function mostrarCartao(indiceCartao){
 
 }
 
-// passo 2 - dar um jeito de identificar o clique do usuario na seta avancar
+// passo 2 - identificar o clique do usuario na seta avancar
 btnAvancar.addEventListener("click", function () {
     if(cartaoAtual === cartoes.length - 1) return;
 
